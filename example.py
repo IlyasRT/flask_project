e48fb07593128f8da729cbc74c2a7a70b73897fb
IlyasRT <razyapovig@gmail.com>
2024-04-11 11:23:53 +0300

    3 commit

--- example.py
+++ example.py
@@ -1,3 +1,4 @@
+
 from flask import Flask
 
 # Это callable WSGI-приложение
@@ -6,3 +7,13 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return '***********\nHELLO to Flask!\n***********'
+    
+
+@app.get('/users')
+def users_get():
+    return 'GET /users'
+
+
+@app.post('/users')
+def users_post():
+    return 'POST /users'
